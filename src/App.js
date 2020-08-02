24128ff132f3d09bce7684a747c239ce63982da1
Stefanc23 <stefano.chr23@gmail.com>
2020-08-02 13:50:55 +0700

    Add cards for cases, recovered, and deaths

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { MenuItem, FormControl, Select } from "@material-ui/core";
+import InfoBox from "./InfoBox";
 import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
 
   useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then(res => res.json())
+      .then(data => setCountryInfo(data));
     const getCountriesData = async () => {
       await fetch("https://disease.sh/v3/covid-19/countries")
         .then(res => res.json())
@@ -23,9 +28,18 @@ function App() {
     getCountriesData();
   }, []);
 
-  const onCountryChange = event => {
+  const onCountryChange = async event => {
     const countryCode = event.target.value;
-    setCountry(countryCode);
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      });
   };
 
   return (
@@ -41,7 +55,26 @@ function App() {
           </Select>
         </FormControl>
       </div>
-      <div className="app__stats"></div>
+      <div className="app__left">
+        <div className="app__stats">
+          <InfoBox
+            title="Coronavirus Cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoBox
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoBox
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
+        </div>
+      </div>
+      <div className="app__right"></div>
     </div>
   );
 }
